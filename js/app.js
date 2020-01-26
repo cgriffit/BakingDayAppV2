69a03041e667e9be0d09d9855dd827d4d97a4f88
cgriffit <chanson1891@gmail.com>
2020-01-26 15:12:29 -0600

    update layout

--- js/app.js
+++ js/app.js
@@ -4,33 +4,24 @@ const openDropdownMenu = () => {
 }
 document.querySelector("#years").addEventListener("click", openDropdownMenu);
 
-// ====== Baked Good Modal ======
-const bakedGoodDialog = document.querySelector("#bakedGoodEntryDialog");
+// ====== Input Event Listener ======
+const bakeInputs = document.querySelectorAll(".groupContainer input");
 
-document.querySelector("#addBtn").addEventListener("click", () => {
-    // open modal
-    bakedGoodDialog.showModal();
-});
+for (let bakeInput of bakeInputs) {
+    bakeInput.addEventListener("keypress", event => {
+        // check if enter was pressed
+        if (event.keyCode === 13 || event.which === 13) {           
+            let bakeName = event.target.value;
+            let bakeListID = event.target.previousElementSibling.id;
+            let markup = "";
 
-const closeModal = () => {
-    // get user input
-    let checked = document.querySelector("input:checked").value;
-    let name = document.querySelector("#bakeName").value;
-
-    // close modal
-    bakedGoodDialog.close([checked, bakeName]);
-    bakedGoodDialog.addEventListener('close', function() {
-        // add input to correct list
-        let bakeType = bakedGoodDialog.returnValue[0];
-        let bakeName = bakedGoodDialog.returnValue[1];
-
-        
-        // display lists
-
+            markup = `
+                <li>${bakeName}</li>
+            `;
+            // add item to the end of the list
+            document.querySelector(`#${bakeListID}`).insertAdjacentHTML("beforeend", markup);
+            // reset input
+            document.querySelector(`#${bakeInput.id}`).value = "";
+        }
     });
-    // reset form
-    document.querySelector("#bakedGoodEntryForm").reset();
 }
-
-document.querySelector("#submitBtn").addEventListener("click", closeModal);
-    
